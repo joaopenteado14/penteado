e1bd67801cc5cbf8dfb631e000fe4f1dccf060a1
joaopenteado14 <penteadojv1314@gmail.com>
2025-09-25 18:01:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -33,19 +33,18 @@ if (process.env.AI_API_KEY) {
 }
 
  Google Calendar
- const oauth2Client = new google.auth.OAuth2(
-//   process.env.GOOGLE_CLIENT_ID,
-//   process.env.GOOGLE_CLIENT_SECRET,
-//   process.env.GOOGLE_REDIRECT_URI
-// );
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.GOOGLE_REDIRECT_URI
+);
+if (process.env.GOOGLE_REFRESH_TOKEN) {
+  oauth2Client.setCredentials({
+    refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+  });
+}
 
-// if (process.env.GOOGLE_REFRESH_TOKEN) {
-   oauth2Client.setCredentials({
-   refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
-//   });
-// }
-
- const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
+const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
 
 // Logger
 const logger = winston.createLogger({
